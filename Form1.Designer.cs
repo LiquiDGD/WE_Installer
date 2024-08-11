44d6fba219cc2b6d1c9c7a668909641d609c1540
LiquiDGD <woodellre@gmail.com>
2024-08-11 14:41:58 -0700

    Design adjustments, add lables, fix windows size.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -53,6 +53,9 @@
             this.buttonAddProcess = new System.Windows.Forms.Button();
             this.buttonRemoveProcess = new System.Windows.Forms.Button();
             this.checkedListBoxCollectors = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // InstallButton
@@ -82,7 +85,7 @@
             // OutputTextBox
             // 
             this.OutputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.OutputTextBox.Location = new System.Drawing.Point(8, 334);
+            this.OutputTextBox.Location = new System.Drawing.Point(8, 382);
             this.OutputTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.OutputTextBox.Multiline = true;
             this.OutputTextBox.Name = "OutputTextBox";
@@ -117,7 +120,7 @@
             // listBoxServicesAvailable
             // 
             this.listBoxServicesAvailable.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.listBoxServicesAvailable.Location = new System.Drawing.Point(8, 133);
+            this.listBoxServicesAvailable.Location = new System.Drawing.Point(8, 168);
             this.listBoxServicesAvailable.Name = "listBoxServicesAvailable";
             this.listBoxServicesAvailable.Size = new System.Drawing.Size(305, 95);
             this.listBoxServicesAvailable.TabIndex = 5;
@@ -125,7 +128,7 @@
             // listBoxServicesSelected
             // 
             this.listBoxServicesSelected.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.listBoxServicesSelected.Location = new System.Drawing.Point(375, 133);
+            this.listBoxServicesSelected.Location = new System.Drawing.Point(375, 168);
             this.listBoxServicesSelected.Name = "listBoxServicesSelected";
             this.listBoxServicesSelected.Size = new System.Drawing.Size(303, 95);
             this.listBoxServicesSelected.TabIndex = 6;
@@ -133,7 +136,7 @@
             // buttonAddService
             // 
             this.buttonAddService.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.buttonAddService.Location = new System.Drawing.Point(321, 133);
+            this.buttonAddService.Location = new System.Drawing.Point(321, 168);
             this.buttonAddService.Name = "buttonAddService";
             this.buttonAddService.Size = new System.Drawing.Size(50, 23);
             this.buttonAddService.TabIndex = 7;
@@ -144,7 +147,7 @@
             // buttonRemoveService
             // 
             this.buttonRemoveService.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.buttonRemoveService.Location = new System.Drawing.Point(319, 162);
+            this.buttonRemoveService.Location = new System.Drawing.Point(319, 197);
             this.buttonRemoveService.Name = "buttonRemoveService";
             this.buttonRemoveService.Size = new System.Drawing.Size(50, 23);
             this.buttonRemoveService.TabIndex = 8;
@@ -155,7 +158,7 @@
             // listBoxProcessesAvailable
             // 
             this.listBoxProcessesAvailable.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.listBoxProcessesAvailable.Location = new System.Drawing.Point(8, 234);
+            this.listBoxProcessesAvailable.Location = new System.Drawing.Point(8, 282);
             this.listBoxProcessesAvailable.Name = "listBoxProcessesAvailable";
             this.listBoxProcessesAvailable.Size = new System.Drawing.Size(305, 95);
             this.listBoxProcessesAvailable.TabIndex = 9;
@@ -163,7 +166,7 @@
             // listBoxProcessesSelected
             // 
             this.listBoxProcessesSelected.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.listBoxProcessesSelected.Location = new System.Drawing.Point(375, 234);
+            this.listBoxProcessesSelected.Location = new System.Drawing.Point(375, 282);
             this.listBoxProcessesSelected.Name = "listBoxProcessesSelected";
             this.listBoxProcessesSelected.Size = new System.Drawing.Size(303, 95);
             this.listBoxProcessesSelected.TabIndex = 10;
@@ -171,7 +174,7 @@
             // buttonAddProcess
             // 
             this.buttonAddProcess.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.buttonAddProcess.Location = new System.Drawing.Point(319, 234);
+            this.buttonAddProcess.Location = new System.Drawing.Point(319, 282);
             this.buttonAddProcess.Name = "buttonAddProcess";
             this.buttonAddProcess.Size = new System.Drawing.Size(50, 23);
             this.buttonAddProcess.TabIndex = 11;
@@ -182,7 +185,7 @@
             // buttonRemoveProcess
             // 
             this.buttonRemoveProcess.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.buttonRemoveProcess.Location = new System.Drawing.Point(319, 263);
+            this.buttonRemoveProcess.Location = new System.Drawing.Point(319, 311);
             this.buttonRemoveProcess.Name = "buttonRemoveProcess";
             this.buttonRemoveProcess.Size = new System.Drawing.Size(50, 23);
             this.buttonRemoveProcess.TabIndex = 12;
@@ -195,19 +198,49 @@
             this.checkedListBoxCollectors.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.checkedListBoxCollectors.CheckOnClick = true;
             this.checkedListBoxCollectors.FormattingEnabled = true;
-            this.checkedListBoxCollectors.Location = new System.Drawing.Point(8, 33);
+            this.checkedListBoxCollectors.Location = new System.Drawing.Point(8, 53);
             this.checkedListBoxCollectors.MultiColumn = true;
             this.checkedListBoxCollectors.Name = "checkedListBoxCollectors";
             this.checkedListBoxCollectors.Size = new System.Drawing.Size(670, 94);
             this.checkedListBoxCollectors.Sorted = true;
             this.checkedListBoxCollectors.TabIndex = 15;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Processes:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Services:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Collectors:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(684, 337);
+            this.ClientSize = new System.Drawing.Size(684, 386);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.checkedListBoxCollectors);
             this.Controls.Add(this.buttonRemoveProcess);
             this.Controls.Add(this.buttonAddProcess);
@@ -223,6 +256,7 @@
             this.Controls.Add(this.CreateConfigButton);
             this.Controls.Add(this.InstallButton);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
@@ -234,5 +268,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
